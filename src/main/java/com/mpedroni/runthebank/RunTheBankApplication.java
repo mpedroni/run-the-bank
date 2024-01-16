96e45cd06a1375cf028405e7d9f67c60fb35abc2
Matheus Pedroni <mpedroni77@gmail.com>
2024-01-15 23:15:40 -0300

    :tada: first commit

---
+++ src/main/java/com/mpedroni/runthebank/RunTheBankApplication.java
@@ -0,0 +1,13 @@
+package com.mpedroni.runthebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunTheBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunTheBankApplication.class, args);
+	}
+
+}
