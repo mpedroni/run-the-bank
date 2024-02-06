cb3053909d5722d1ada1b53d63971e87e19c3318
Matheus Pedroni <mpedroni77@gmail.com>
2024-02-05 23:46:20 -0300

    fix: update transaction repository identifier type

--- src/main/java/com/mpedroni/runthebank/infra/transaction/persistence/TransactionRepository.java
+++ src/main/java/com/mpedroni/runthebank/infra/transaction/persistence/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.mpedroni.runthebank.infra.transaction.persistence;
 
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<TransactionJpaEntity, String> {
+public interface TransactionRepository extends JpaRepository<TransactionJpaEntity, UUID> {
 }
