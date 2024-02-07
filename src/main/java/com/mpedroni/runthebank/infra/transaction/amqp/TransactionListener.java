b21c6f209c62804081e1477752e75bd2ef6f52ac
Matheus Pedroni <mpedroni77@gmail.com>
2024-02-07 01:35:01 -0300

    feat: notify clients after successful payment

--- src/main/java/com/mpedroni/runthebank/infra/transaction/amqp/TransactionListener.java
+++ src/main/java/com/mpedroni/runthebank/infra/transaction/amqp/TransactionListener.java
@@ -5,6 +5,7 @@ import com.mpedroni.runthebank.domain.transaction.Transaction;
 import com.mpedroni.runthebank.domain.transaction.TransactionGateway;
 import com.mpedroni.runthebank.domain.transaction.TransactionType;
 import com.mpedroni.runthebank.domain.transaction.events.TransactionCreatedEvent;
+import com.mpedroni.runthebank.infra.services.NotificationService;
 import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
@@ -15,10 +16,13 @@ import org.springframework.stereotype.Service;
 public class TransactionListener {
     private final TransactionGateway transactionGateway;
     private final AccountService accountService;
+    private final NotificationService notificationService;
 
-    public TransactionListener(TransactionGateway transactionGateway, AccountService accountService) {
+    public TransactionListener(TransactionGateway transactionGateway, AccountService accountService,
+        NotificationService notificationService) {
         this.transactionGateway = transactionGateway;
         this.accountService = accountService;
+        this.notificationService = notificationService;
     }
 
     @RabbitListener(
@@ -28,7 +32,7 @@ public class TransactionListener {
     )
     public void transactionCreated(TransactionCreatedEvent event, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) {
         try {
-            var transaction = transactionGateway.findById(event.id()).orElseThrow(() -> new RuntimeException("Transaction not found"));
+            var transaction = transactionGateway.findById(event.id()).orElseThrow(() -> new RuntimeException("Transaction not found. Id: " + event.id()));
 
             var type = transaction.type();
 
@@ -37,8 +41,27 @@ public class TransactionListener {
             else throw new IllegalStateException("Unknown transaction type");
 
             channel.basicAck(tag, false);
+            notifyClients(transaction);
         } catch (Exception e) {
             System.out.println("Failed to process the message: " + e.getMessage());
+        }
+    }
+
+    private void notifyClients(Transaction transaction) {
+        String payeeMessage = "";
+        String payerMessage = "";
+
+        if (transaction.type() == TransactionType.DEPOSIT) {
+            payeeMessage = "Seu depósito de " + transaction.amount() + " foi realizado com sucesso";
+        } else {
+            payeeMessage = "Você recebeu uma transferência de " + transaction.amount();
+            payerMessage = "Sua transferência de " + transaction.amount() + " foi realizada com sucesso";
+        }
+
+        notificationService.notify(transaction.payee().id(), payeeMessage);
+
+        if (!payerMessage.isEmpty()) {
+            notificationService.notify(transaction.payer().id(), payerMessage);
         }
     }
 
