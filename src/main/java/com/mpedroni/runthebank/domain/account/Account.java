0b109e97fcfb0bbad158973dd13e99724d252b4a
Matheus Pedroni <mpedroni77@gmail.com>
2024-02-08 20:19:17 -0300

    refactor: convert account model to class

--- src/main/java/com/mpedroni/runthebank/domain/account/Account.java
+++ src/main/java/com/mpedroni/runthebank/domain/account/Account.java
@@ -1,16 +1,34 @@
 package com.mpedroni.runthebank.domain.account;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.UUID;
 
-public record Account(
-    UUID id,
-    UUID clientId,
-    int agency,
-    int number,
-    BigDecimal balance,
-    AccountStatus status
-) {
+public final class Account {
+
+    private final UUID id;
+    private final UUID clientId;
+    private final int agency;
+    private final int number;
+    private final BigDecimal balance;
+    private AccountStatus status;
+
+    public Account(
+        UUID id,
+        UUID clientId,
+        int agency,
+        int number,
+        BigDecimal balance,
+        AccountStatus status
+    ) {
+        this.id = id;
+        this.clientId = clientId;
+        this.agency = agency;
+        this.number = number;
+        this.balance = balance;
+        this.status = status;
+    }
+
     public Account(
         UUID id,
         UUID clientId,
@@ -26,7 +44,8 @@ public record Account(
         int agency,
         int number
     ) {
-        return new Account(UUID.randomUUID(), clientId, agency, number, BigDecimal.ZERO, AccountStatus.ACTIVE);
+        return new Account(UUID.randomUUID(), clientId, agency, number, BigDecimal.ZERO,
+            AccountStatus.ACTIVE);
     }
 
     public static Account restore(
@@ -43,4 +62,62 @@ public record Account(
     public boolean isActive() {
         return status == AccountStatus.ACTIVE;
     }
+
+    public UUID id() {
+        return id;
+    }
+
+    public UUID clientId() {
+        return clientId;
+    }
+
+    public int agency() {
+        return agency;
+    }
+
+    public int number() {
+        return number;
+    }
+
+    public BigDecimal balance() {
+        return balance;
+    }
+
+    public AccountStatus status() {
+        return status;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (Account) obj;
+        return Objects.equals(this.id, that.id) &&
+            Objects.equals(this.clientId, that.clientId) &&
+            this.agency == that.agency &&
+            this.number == that.number &&
+            Objects.equals(this.balance, that.balance) &&
+            Objects.equals(this.status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, clientId, agency, number, balance, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Account[" +
+            "id=" + id + ", " +
+            "clientId=" + clientId + ", " +
+            "agency=" + agency + ", " +
+            "number=" + number + ", " +
+            "balance=" + balance + ", " +
+            "status=" + status + ']';
+    }
+
 }
